98365f8853834a640076c83954c1831a68e1d23b
woojun <woojs1209@naver.com>
2021-03-10 19:45:53 +0900

    create all models

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,19 @@ DJANGO_APPS  = [
 ]
 
 PROJECT_APPS = [
+    "core.apps.CoreConfig",
     "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
+    
+]
+
+# library
+THIRD_PARTY_APPS = [
+    "django-countries",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
