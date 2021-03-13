79e185876632226594d011a20e985baa5a54d954
woojun <woojs1209@naver.com>
2021-03-13 16:44:17 +0900

    Home View

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from . import models 
 
-# Create your views here.
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    context_object_name = "rooms"
