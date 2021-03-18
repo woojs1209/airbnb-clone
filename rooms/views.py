9d3022d9c95bac021fa4ea84736e0ee12d036fcd
woojun <woojs1209@naver.com>
2021-03-18 13:32:54 +0900

    room views

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models 
 
 class HomeView(ListView):
@@ -8,3 +8,7 @@ class HomeView(ListView):
     model = models.Room
     paginate_by = 10
     context_object_name = "rooms"
+
+class RoomDetail(DetailView):
+    
+    model = models.Room
